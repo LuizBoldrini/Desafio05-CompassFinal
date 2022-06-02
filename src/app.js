9db5ad6bbc2bce1a2e51398794cc63f37efaa68b
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-02 12:03:27 -0300

    feat: Created connection with the database and the schema of Car

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const database = require("./app/config/dbConnect.js");
+
+database.on("error", console.log.bind(console, "DB não conectado"));
+database.once("open", () => {console.log("Conexão no BD feita com sucesso");});
 
 const app = express();
 app.use(express.json());
---
+++ src/app/models/Car.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const CarSchema = new mongoose.Schema({
+	model: {
+		type: String,
+		required: true
+	},
+	type: {
+		type: String,
+		required: true
+	},
+	brand: {
+		type: String,
+		required: true
+	},
+	color: {
+		type: String,
+		required: true
+	},
+	year: {
+		type: Date,
+		required: true
+	},
+	accessories: [{
+		type: String,
+		required: true
+	}],
+	passengersQtd: {
+		type: Number,
+		required: true
+	}
+});
+
+const Car = mongoose.model("Car", CarSchema);
+
+module.exports = Car;
