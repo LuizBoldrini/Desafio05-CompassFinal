78b925d5d2e5a7c1eb6005c5590119aab6bf1637
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-04 13:55:04 -0300

    fix: Added validation in Post route

--- src/app/routes/PersonRoutes.js
+++ src/app/routes/PersonRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const PersonController = require("../controllers/PersonController");
+const ValidarPerson = require("../middleware/ValidarPerson");
 
 const router = express.Router();
 
 router
-	.post("/api/v1/person", PersonController.criaPerson)
+	.post("/api/v1/person", ValidarPerson,PersonController.criaPerson)
 	.get("/api/v1/person", PersonController.listaPerson)
 	.get("/api/v1/person/:id", PersonController.listaPersonPorId)
 	.put("/api/v1/person/:id", PersonController.atualizaPerson)
