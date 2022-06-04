0fd00fee32f309cf0f2a7c2510629514397aba14
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-04 16:48:45 -0300

    fix: Added ValidarCar in Post route

--- src/app/routes/CarRoutes.js
+++ src/app/routes/CarRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const CarController = require("../controllers/CarController");
+const ValidarCar = require("../middleware/ValidarCar");
 
 const router = express.Router();
 
 router
-	.post("/api/v1/car", CarController.criaCarro)
+	.post("/api/v1/car",ValidarCar ,CarController.criaCarro)
 	.get("/api/v1/car", CarController.listaCarros)
 	.get("/api/v1/car/:id", CarController.listaCarroPorId)
 	.put("/api/v1/car/:id", CarController.atualizaCarro)
