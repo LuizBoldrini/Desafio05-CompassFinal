7e68dc3a9d9d11ed43217c0b95e0fdc9a4e2f93e
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-04 15:22:30 -0300

    fix/feat: Changed error folder name to utils and created function to form cpf

---
+++ src/app/utils/FormataCpf.js
@@ -0,0 +1,15 @@
+function formataCpf(funcionario) {
+	if(Array.isArray(funcionario)) {
+		const funcionarioFormatado = funcionario.map(teste => {
+			const cpf = teste.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})$/, "$1.$2.$3-$4");
+			teste.cpf = cpf;
+		});
+		return funcionarioFormatado;
+	} else {
+		const cpf = funcionario.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})$/, "$1.$2.$3-$4");
+		const funcionarioFormatado = funcionario.cpf = cpf;
+		return funcionarioFormatado;
+	}
+}
+
+module.exports = formataCpf;
