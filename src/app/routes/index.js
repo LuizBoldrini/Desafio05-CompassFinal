adb54052d0637bc330579c49cb64ff8273b1a908
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-05 03:01:35 -0300

    fix: Added authentication route in index

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,9 +1,11 @@
 const CarRouter = require("./CarRoutes");
 const PersonRoutes = require("./PersonRoutes");
+const AuthenticateRoute = require("./AuthenticateRoute");
 
 const routes = (app) => {
 	app.use(CarRouter);
 	app.use(PersonRoutes);
+	app.use(AuthenticateRoute);
 };
 
 module.exports = routes;
