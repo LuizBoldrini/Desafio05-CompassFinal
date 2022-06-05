0a3610ea3564f7fe1a43294eecbc46ef6768a564
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-05 03:00:15 -0300

    feat:Created controller for Authenticate

---
+++ src/app/controllers/AuthenticateController.js
@@ -0,0 +1,17 @@
+const AuthenticateService = require("../services/AuthenticateService");
+
+class AuthController {
+	async criaAut(req, res) {
+		try {
+			const { email, password } = req.body;
+			const result = await AuthenticateService.acessa(email, password);
+			return res.status(201).json(result);
+		} catch (error) {
+			return res.status(400).json({
+				error: error.message
+			});
+		}
+	}
+}
+
+module.exports = new AuthController();
