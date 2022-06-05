9e01d2705aad7accddde3fd26b6537963c7a986e
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-05 02:58:46 -0300

    feat:Created validator for Authenticate

---
+++ src/app/middleware/ValidarAut.js
@@ -0,0 +1,20 @@
+const joi = require("joi");
+const InvalidField = require("../utils/InvalidField");
+
+const autPost = joi.object({
+	email: joi.string().email().required().error(new InvalidField("email")),
+	password: joi.string().min(6).required().error(new InvalidField("passowrd")),
+});
+
+module.exports =async (req, res, next) => {
+	const reqBody = req.body;
+	try{
+		if(req.method == "POST") {
+			await autPost.validateAsync({...reqBody});
+			next();
+		}
+
+	} catch(error) {
+		res.status(400).json(error.message);
+	}
+};
