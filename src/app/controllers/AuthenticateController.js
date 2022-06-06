16af384e3a50830636772580c37c707ae50e688e
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-05 21:47:19 -0300

    fix: Changed authentication status code

--- src/app/controllers/AuthenticateController.js
+++ src/app/controllers/AuthenticateController.js
@@ -5,7 +5,7 @@ class AuthController {
 		try {
 			const { email, password } = req.body;
 			const result = await AuthenticateService.acessa(email, password);
-			return res.status(201).json(result);
+			return res.status(204).json(result);
 		} catch (error) {
 			return res.status(400).json({
 				error: error.message
