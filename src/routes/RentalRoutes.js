64a4a171cd357998356bd209e8bde3c435a17a51
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-11 20:23:09 -0300

    feat: Created routes for rental

--- src/routes/RentalRoutes.js
+++ src/routes/RentalRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const RentalController = require("../app/controllers/RentalController");
+const ValidarRental = require("../app/middleware/ValidarRental");
 
 const router = express.Router();
 
 router
-	.post("/api/v1/rental", RentalController.criaRental)
+	.post("/api/v1/rental", ValidarRental,RentalController.criaRental)
 	.get("/api/v1/rental", RentalController.listaRental)
 	.get("/api/v1/rental/:id", RentalController.listaRentalPorId)
-	.put("/api/v1/rental/:id", RentalController.atualizaRental)
+	.put("/api/v1/rental/:id", ValidarRental,RentalController.atualizaRental)
 	.delete("/api/v1/rental/:id", RentalController.deletaRental);
 
 module.exports = router;
