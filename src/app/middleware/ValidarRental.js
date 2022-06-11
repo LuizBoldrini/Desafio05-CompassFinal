e89652c1c9ac13f40d6b131ae930364fc67881d2
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-11 20:21:17 -0300

    feat: Created validation for rental

---
+++ src/app/middleware/ValidarRental.js
@@ -0,0 +1,58 @@
+const joi = require("joi");
+
+const rentalPost = joi.object({
+	name: joi.string().min(2).required(),
+	cnpj: joi.string().min(18).required(),
+	activities: joi.string().min(5).required(),
+	address: joi.array().min(1).items(
+		{
+			zipCode: joi.string().min(8).max(8).required(),
+			street: joi.string().min(2),
+			complement: joi.string(),
+			number: joi.number().min(1).required(),
+			city: joi.string().min(1),
+			state: joi.string().min(2).max(2),
+			isFilial: joi.boolean().required()
+		}
+	)
+});
+
+const rentalPut = joi.object({
+	name: joi.string().min(2),
+	cnpj: joi.string().min(18),
+	activities: joi.string().min(5),
+	address: joi.array().min(1).items(
+		{
+			zipCode: joi.string().min(8).max(8),
+			street: joi.string().min(2),
+			complement: joi.string(),
+			number: joi.number().min(1),
+			city: joi.string().min(1),
+			state: joi.string().min(2).max(2),
+			isFilial: joi.boolean()
+		}
+	)
+});
+
+module.exports =async (req, res, next) => {
+	const reqBody = req.body;
+
+	try {
+		if(req.method == "POST") {
+			await rentalPost.validateAsync({...reqBody });
+			next();
+		}
+
+		if(req.method == "PUT") {
+			await rentalPut.validateAsync({...reqBody });
+			next();
+		}
+	} catch (error) {
+		return res.status(400).json(
+			error.details.map((detail) => ({
+				name: detail.path.join(),
+				description: detail.message
+			})));}
+
+
+};
