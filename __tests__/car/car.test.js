c03970495440c6b76696a8212fe88d8d05d8992c
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-12 16:35:27 -0300

    feat: Created tests for car route

--- __tests__/car/car.test.js
+++ __tests__/car/car.test.js
@@ -14,27 +14,102 @@ beforeEach(async () => {
 describe("Create a new car", () => {
 	it("should create a car and return status 201", async () => {
 		const car = {
-			model: "S10 2.8",
+			model: "S10 3.0",
 			type: "sedan",
 			brand: "GM",
-			color: "branco",
-			year: "2008",
+			color: "prata",
+			year: "2003",
 			accessories: [
 				{
-					description: "Ar-condicionado"
+					description: "Gabine dupla"
 				},
 				{
 					description: "Dir. Hidráulica"
-				},
-				{
-					description: "Cabine Dupla"
-				},
+				}
 			],
-			passengersQtd: 5
+			passengersQtd: 6
 		};
 		const response = await request(app).post("/api/v1/car").send(car);
-		const { body } = response;
-		expect(body.model).toBe("S10 2.8");
 		expect(response.statusCode).toBe(201);
+	});
+});
+
+describe("List a car", () => {
+	it("must create a car and list it returning a status of 200", async () => {
+		const car = {
+			model: "S10 3.0",
+			type: "sedan",
+			brand: "GM",
+			color: "prata",
+			year: "2003",
+			accessories: [
+				{
+					description: "Gabine dupla"
+				},
+				{
+					description: "Dir. Hidráulica"
+				}
+			],
+			passengersQtd: 6
+		};
+		await request(app).post("/api/v1/car").send(car);
+		const response = await request(app).get("/api/v1/car/");
+		expect(response.statusCode).toBe(200);
+	});
+});
+
+describe("Get car by id", () => {
+	it("should get car by Id, returning a status of 200", async () => {
+		const car = {
+			model: "S10 3.0",
+			type: "sedan",
+			brand: "GM",
+			color: "prata",
+			year: "2003",
+			accessories: [
+				{
+					description: "Gabine dupla"
+				},
+				{
+					description: "Dir. Hidráulica"
+				}
+			],
+			passengersQtd: 6
+		};
+		const { text } = await request(app).post("/api/v1/car/").send(car);
+
+		const { _id } = JSON.parse(text);
+
+		const response = await request(app).get(`/api/v1/car/${_id.toString()}`);
+		const { status } = response;
+		expect(status).toBe(200);
+	});
+});
+
+describe("Delete car by id", () => {
+	it("should delete car by Id", async () => {
+		const car = {
+			model: "S10 3.0",
+			type: "sedan",
+			brand: "GM",
+			color: "prata",
+			year: "2003",
+			accessories: [
+				{
+					description: "Gabine dupla"
+				},
+				{
+					description: "Dir. Hidráulica"
+				}
+			],
+			passengersQtd: 6
+		};
+		const { text } = await request(app).post("/api/v1/car/").send(car);
+
+		const { _id } = JSON.parse(text);
+
+		const response = await request(app).delete(`/api/v1/car/${_id.toString()}`);
+		const { status } = response;
+		expect(status).toBe(204);
 	});
 });
