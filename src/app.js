f35316a8ec2732e70df834ddd51382b586376ca8
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-12 00:58:15 -0300

    feat: Implemented swagger in API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const database = require("./app/config/dbConnect.js");
 const routes = require("./routes/index");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 
 database.on("error", console.log.bind(console, "DB não conectado"));
 database.once("open", () => {console.log("Conexão no BD feita com sucesso");});
@@ -8,6 +10,7 @@ database.once("open", () => {console.log("Conexão no BD feita com sucesso");});
 const app = express();
 app.use(express.json());
 
+app.use("/api/v1/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 routes(app);
 
 module.exports = app;
