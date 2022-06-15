9a8d01d438386b339615ba8860bebfd72b295175
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-15 14:28:11 -0300

    feat: Implemented car route authentication

--- src/routes/CarRoutes.js
+++ src/routes/CarRoutes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const CarController = require("../app/controllers/CarController");
 const ValidarCar = require("../app/middleware/ValidarCar");
+const Auth = require("../app//middleware/Auth");
 
 const router = express.Router();
 
 router
-	.post("/api/v1/car",ValidarCar ,CarController.createCar)
-	.get("/api/v1/car", CarController.listCar)
-	.get("/api/v1/car/:id", CarController.listById)
-	.put("/api/v1/car/:id",ValidarCar ,CarController.updateCar)
-	.delete("/api/v1/car/:id", CarController.deleteCar)
-	.patch("/api/v1/car/:id/acessorios/:idAcess", CarController.updateDesc);
+	.post("/api/v1/car", Auth, ValidarCar ,CarController.createCar)
+	.get("/api/v1/car", Auth, CarController.listCar)
+	.get("/api/v1/car/:id", Auth,CarController.listById)
+	.put("/api/v1/car/:id", Auth,ValidarCar ,CarController.updateCar)
+	.delete("/api/v1/car/:id", Auth, CarController.deleteCar)
+	.patch("/api/v1/car/:id/acessorios/:idAcess", Auth, CarController.updateDesc);
 
 
 module.exports = router;
