2176e6ad19fd8bd745dd13d8ac5bbf1987a97dd3
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-16 13:08:28 -0300

    fix: Implemented validations for Person get route

--- src/routes/PersonRoutes.js
+++ src/routes/PersonRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router
 	.post("/api/v1/person", ValidarPerson,PersonController.createPerso)
-	.get("/api/v1/person", PersonController.listPerson)
-	.get("/api/v1/person/:id", PersonController.listById)
+	.get("/api/v1/person", ValidarPerson, PersonController.listPerson)
+	.get("/api/v1/person/:id", ValidarPerson, PersonController.listById)
 	.put("/api/v1/person/:id", ValidarPerson, PersonController.updatePerson)
 	.delete("/api/v1/person/:id", PersonController.deletePerson);
 
