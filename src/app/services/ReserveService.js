e7f8df011c0e1edf1e74bb2d91985876e523d28a
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-18 13:10:19 -0300

    feat: Implemented function to calculate reservation value and validation to only be reserved for those who can drive

--- src/app/services/ReserveService.js
+++ src/app/services/ReserveService.js
@@ -1,10 +1,29 @@
 const ReserveRepository = require("../repository/ReserveRepository");
 const NotFound = require("../erros/NotFound");
+const CalcFinalValue = require("../utils/CalcFinalValue");
+const FleetRepository = require("../repository/FleetRepository");
+const PersonRepository = require("../repository/PersonRepository");
+const CanDrive = require("../erros/CanDrive");
 
 class ReserveService {
 	static async create(payload) {
-		const resultado = await ReserveRepository.create(payload);
-		return resultado;
+		const { id_user } = payload;
+		const user = await PersonRepository.listById(id_user);
+		if (user.canDrive !== "yes") {
+			throw new CanDrive;}
+
+		const { id_car } = payload;
+		const fleet = await FleetRepository.listById(id_car);
+		if (!fleet){
+			throw new NotFound("Fleet");
+		} 
+		const { data_start, data_end } = payload;
+		const { daily_value } = fleet;
+		
+		payload.final_value = CalcFinalValue(data_start, data_end, daily_value);
+
+		
+		return ReserveRepository.create(payload);
 	}
 
 	static async list(payload) {
