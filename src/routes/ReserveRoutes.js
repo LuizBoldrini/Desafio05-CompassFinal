b3cf0dcf05972aff6bb9f5c4e52ae2d5b3d6400f
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-20 00:31:57 -0300

    fix: Implement validation

--- src/routes/ReserveRoutes.js
+++ src/routes/ReserveRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const ReserveController = require("../app/controllers/ReserveController");
+const ValidarReserve = require("../app/middleware/ValidarReserve");
 
 const router = express.Router();
 
 router
-	.post("/api/v1/rental/:id_rental/reserve", ReserveController.createReserve)
+	.post("/api/v1/rental/:id_rental/reserve", ValidarReserve, ReserveController.createReserve)
 	.get("/api/v1/rental/:id_rental/reserve", ReserveController.listReserve)
 	.get("/api/v1/rental/:id_rental/reserve/:id", ReserveController.listById)
 	.put("/api/v1/rental/:id_rental/reserve/:id", ReserveController.updateReserve)
