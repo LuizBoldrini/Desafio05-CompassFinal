c1271be082256398eb027dcb40ae94fea741bdf0
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-20 13:51:19 -0300

    fix: modified test

--- __tests__/person/person.test.js
+++ __tests__/person/person.test.js
@@ -9,48 +9,36 @@ beforeEach(async () => {
 	await Person.deleteMany();
 });
 
+let person = {
+	name: "João da Silva",
+	cpf: "21120038014",
+	birthDay: "10/10/2003",
+	email: "joazinho@email.com",
+	password: "123457",
+	canDrive: "yes"
+};
+
 describe("Create a new person", () => {
 	it("should create a person and return status 201", async () => {
-		const person = {
-			name: "Joãozinho da Silva",
-			cpf: "04126271244",
-			birthDay: "10-10-2002",
-			email: "joazinho@emai.com",
-			password: "123456",
-			canDrive: "yes"
-		};
-		const response = await request(app).post("/api/v1/person").send(person);
+		const Person = person;
+		const response = await request(app).post("/api/v1/person").send(Person);
 		expect(response.statusCode).toBe(201);
 	});
 });
 
-describe("List all person", () => {
-	it("must create a person and list it returning a status of 200", async () => {
-		const person = {
-			name: "Joãozinho da Silva",
-			cpf: "04126271244",
-			birthDay: "10-10-2002",
-			email: "joazinho@emai.com",
-			password: "123456",
-			canDrive: "yes"
-		};
-		await request(app).post("/api/v1/person").send(person);
-		const response = await request(app).get("/api/v1/person/");
+describe("Get all people", () => {
+	it("should return status code 200", async () => {
+		const Person = person;
+		await request(app).post("/api/v1/person").send(Person);
+		const response = await request(app).get("/api/v1/person");
 		expect(response.statusCode).toBe(200);
 	});
 });
 
 describe("Get person by id", () => {
 	it("should get person by Id, returning a status of 200", async () => {
-		const person = {
-			name: "Joãozinho da Silva",
-			cpf: "04126271244",
-			birthDay: "10-10-2002",
-			email: "joazinho@emai.com",
-			password: "123456",
-			canDrive: "yes"
-		};
-		const { text } = await request(app).post("/api/v1/person/").send(person);
+		const Person = person;
+		const { text } = await request(app).post("/api/v1/person/").send(Person);
 		const { _id } = JSON.parse(text);
 		const response = await request(app).get(`/api/v1/person/${_id.toString()}`);
 		expect(response.statusCode).toBe(200);
@@ -59,15 +47,8 @@ describe("Get person by id", () => {
 
 describe("Delete person by id", () => {
 	it("should delete person by Id", async () => {
-		const person = {
-			name: "Joãozinho da Silva",
-			cpf: "04126271244",
-			birthDay: "10-10-2002",
-			email: "joazinho@emai.com",
-			password: "123456",
-			canDrive: "yes"
-		};
-		const { text } = await request(app).post("/api/v1/person/").send(person);
+		const Person = person;
+		const { text } = await request(app).post("/api/v1/person/").send(Person);
 		const { _id } = JSON.parse(text);
 		const response = await request(app).delete(`/api/v1/person/${_id.toString()}`);
 		expect(response.statusCode).toBe(204);
