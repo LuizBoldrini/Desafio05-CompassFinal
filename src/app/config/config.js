e8413476f1913278df61bd2490f7c855a8a12fca
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-20 13:52:04 -0300

    fix: Modified config.js

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,4 +1,8 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+dotenv.config({
+	path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
 
 module.exports = {
 	database: {
