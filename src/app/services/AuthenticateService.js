e0ba3157fcaeaadb0dd85b0fe7b5f526d8834757
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-21 11:46:29 -0300

    fix: eslint airb changes

--- src/app/services/AuthenticateService.js
+++ src/app/services/AuthenticateService.js
@@ -1,9 +1,9 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const PersonRepository = require("../repository/PersonRepository");
 const NotFound = require("../erros/NotFound");
 const PassIncorrect = require("../erros/PassIncorrect");
 const formataCpf = require("../utils/FormataCpf");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const authConfig = require("../config/authConfig.json");
 require("dotenv").config();
 
@@ -17,7 +17,7 @@ class AuthenticateService {
 		const { canDrive } = user;
 
 		if(!(await bcrypt.compare(password, user.password))) {
-			new PassIncorrect("password");
+			throw new PassIncorrect("password");
 		}	
 		user.password = undefined;
 
