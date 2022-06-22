bf6962787a4b6fd85c934c3f28e8c0efdea8518f
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-21 21:10:09 -0300

    fix: config heroku

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const app = require("./app.js");
 require("dotenv").config();
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(process.env.APP_URL || port , () => { console.log(`Servidor na porta: ${port}`);});
+app.listen(port , () => { console.log(`Servidor na porta: ${port}`);});
