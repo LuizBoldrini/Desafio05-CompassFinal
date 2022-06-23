1707bfb1af8239c248b5db92b3770f18fe97f8bb
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-23 11:42:18 -0300

    fix: Refactored changes

--- src/routes/FleetRoutes.js
+++ src/routes/FleetRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const FleetController = require("../app/controllers/FleetController");
-const ValidarFleet = require("../app/middleware/ValidarFleet");
+const ValidFleet = require("../app/middleware/ValidFleet");
 
 const router = express.Router();
 
 router
-	.post("/api/v1/rental/:id_rental/fleet", ValidarFleet, FleetController.createReserve)
-	.get("/api/v1/rental/:id_rental/fleet", ValidarFleet, FleetController.listReserve)
-	.get("/api/v1/rental/:id_rental/fleet/:id", ValidarFleet, FleetController.listById)
-	.put("/api/v1/rental/:id_rental/fleet/:id", ValidarFleet, FleetController.updateReserve)
-	.delete("/api/v1/rental/:id_rental/fleet/:id", ValidarFleet, FleetController.deleteReserve);
+	.post("/api/v1/rental/:id_rental/fleet", ValidFleet, FleetController.createReserve)
+	.get("/api/v1/rental/:id_rental/fleet", ValidFleet, FleetController.listReserve)
+	.get("/api/v1/rental/:id_rental/fleet/:id", ValidFleet, FleetController.listById)
+	.put("/api/v1/rental/:id_rental/fleet/:id", ValidFleet, FleetController.updateReserve)
+	.delete("/api/v1/rental/:id_rental/fleet/:id", FleetController.deleteReserve);
 
 module.exports = router;
