073538dabfa1f36df3a9b4efc651d8f2a4bfc3b6
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-23 10:05:08 -0300

    fix: Changed swagger path

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const database = require("./app/config/dbConnect.js");
-const routes = require("./routes/index");
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger.json");
+const database = require("./app/config/dbConnect");
+const routes = require("./routes/index");
+const swaggerDocument = require("./app/docs/swagger.json");
 
 database.on("error", console.log.bind(console, "DB não conectado"));
 database.once("open", () => {console.log("Conexão no BD feita com sucesso");});
