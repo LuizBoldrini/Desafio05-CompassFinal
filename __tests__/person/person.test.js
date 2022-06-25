c183710e36326403630b4cb5f7f6d12e99d5ceab
luiz Boldrini <luizboldriniej@gmail.com>
2022-06-25 15:27:53 -0300

    fix:Implemented tests in the person route

--- __tests__/person/person.test.js
+++ __tests__/person/person.test.js
@@ -2,55 +2,210 @@ const request = require('supertest');
 const app = require('../../src/app');
 const Person = require('../../src/app/models/Person');
 
-beforeAll(async () => {
-  await Person.deleteMany();
-});
-beforeEach(async () => {
-  await Person.deleteMany();
-});
-
-const person = {
-  name: 'João da Silva',
-  cpf: '21120038014',
-  birthDay: '10/10/2003',
-  email: 'joazinho@email.com',
-  password: '123457',
-  canDrive: 'yes'
-};
-
-describe('Create a new person', () => {
-  it('should create a person and return status 201', async () => {
-    const Person = person;
-    const response = await request(app).post('/api/v1/person').send(Person);
-    expect(response.statusCode).toBe(201);
+describe('Person route test', () => {
+  beforeEach(async () => {
+    await Person.deleteMany();
   });
-});
 
-describe('Get all people', () => {
-  it('should return status code 200', async () => {
-    const Person = person;
-    await request(app).post('/api/v1/person').send(Person);
-    const response = await request(app).get('/api/v1/person');
-    expect(response.statusCode).toBe(200);
+  it('Create a person and register in the database', async () => {
+    const personPost = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    expect(personPost.statusCode).toBe(201);
   });
-});
-
-describe('Get person by id', () => {
-  it('should get person by Id, returning a status of 200', async () => {
-    const Person = person;
-    const { text } = await request(app).post('/api/v1/person/').send(Person);
-    const { _id } = JSON.parse(text);
-    const response = await request(app).get(`/api/v1/person/${_id.toString()}`);
-    expect(response.statusCode).toBe(200);
+  it('Returning error in name', async () => {
+    const personPost = await request(app).post('/api/v1/person').send({
+      name: 'J',
+      cpf: '88943098006',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    expect(personPost.statusCode).toBe(400);
   });
-});
+  it('Returning error in cpf', async () => {
+    const personPost = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '8894309801',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    expect(personPost.statusCode).toBe(400);
+  });
+  it('Returning error in birthDay', async () => {
+    const personPost = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2008',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    expect(personPost.statusCode).toBe(400);
+  });
+  it('Returning error in email', async () => {
+    const personPost = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2008',
+      email: 'joaoSilvaemail.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    expect(personPost.statusCode).toBe(400);
+  });
+  it('Returning error in password', async () => {
+    const personPost = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2008',
+      email: 'joaoSilva@email.com',
+      password: '12345',
+      canDrive: 'yes'
+    });
+    expect(personPost.statusCode).toBe(400);
+  });
+  it('Returning error in canDrive', async () => {
+    const personPost = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2000',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yess'
+    });
+    expect(personPost.statusCode).toBe(400);
+  });
 
-describe('Delete person by id', () => {
-  it('should delete person by Id', async () => {
-    const Person = person;
-    const { text } = await request(app).post('/api/v1/person/').send(Person);
-    const { _id } = JSON.parse(text);
-    const response = await request(app).delete(`/api/v1/person/${_id.toString()}`);
-    expect(response.statusCode).toBe(204);
+  it('Returns all person', async () => {
+    const personGet = await request(app).get('/api/v1/person').send();
+    expect(personGet.status).toBe(200);
+  });
+  it('Returns all people with with error', async () => {
+    const personGet = await request(app).get('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2008',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yess'
+    });
+    expect(personGet.status).toBe(400);
+  });
+
+  it('Returns a person by id', async () => {
+    const person = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    const people = await request(app).get(`/api/v1/person/${person.body._id}`).send();
+    expect(people.status).toBe(200);
+  });
+  it('Returns a person by id with error ', async () => {
+    const person = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '8894309800',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    const people = await request(app).get(`/api/v1/person/${person.body._id}`).send();
+    expect(people.status).toBe(400);
+  });
+
+  it('Change some data of a person', async () => {
+    const person = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    const updatePerson = await request(app).put(`/api/v1/person/${person.body._id}`).send({
+      name: 'João da Silva Neto'
+    });
+    expect(updatePerson.status).toBe(200);
+  });
+  it('Change some data of a person with error', async () => {
+    const person = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    const updatePerson = await request(app).put(`/api/v1/person/${person.body._id}`).send({
+      name: 'J'
+    });
+    expect(updatePerson.status).toBe(400);
+  });
+  it('Delete a person by id with non-standard id error', async () => {
+    await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    const id = '62b25ffae2f0ef696a7ba37';
+    const people = await request(app).put(`/api/v1/person/${id}`).send();
+    expect(people.status).toBe(400);
+  });
+
+  it('Delete a person by id', async () => {
+    const person = await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    const people = await request(app).delete(`/api/v1/person/${person.body._id}`).send();
+    expect(people.status).toBe(200);
+  });
+  it('Delete a person by idDelete a person by id with error of not found', async () => {
+    await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    const id = '62b25ffae2f0ef696a7ba37d';
+    const people = await request(app).delete(`/api/v1/person/${id}`).send();
+    expect(people.status).toBe(400);
+  });
+  it('Delete a person by id with non-standard id error', async () => {
+    await request(app).post('/api/v1/person').send({
+      name: 'João da Silva',
+      cpf: '88943098006',
+      birthDay: '10/08/2001',
+      email: 'joaoSilva@email.com',
+      password: '123456',
+      canDrive: 'yes'
+    });
+    const id = '62b25ffae2f0ef696a7ba37';
+    const people = await request(app).delete(`/api/v1/person/${id}`).send({
+      name: 'João Neto da Silva'
+    });
+    expect(people.status).toBe(400);
   });
 });
